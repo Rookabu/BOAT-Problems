cc3e1f4c10f2b09a9ae8864f125d92733baed7a2
Kevin F <Freymaurer@gmx.de>
2024-09-30 16:10:47 +0200

    fix addAnnotation

--- src/View.fs
+++ src/View.fs
@@ -42,28 +42,20 @@ open Types
 module ModalContext=
     let createModalContext:Fable.React.IContext<DropdownModal> = React.createContext(name="modal") //makes context
 
-
-// module ContextHelper =
-
-//   let addAnnotation =
-//       (fun (e:MouseEvent) ->
-//           let term = window.getSelection().ToString().Trim()
-//           if term.Length <> 0 then 
-//               let newAnno = (AnnotationState) //errors are defined in Builder.fs
-//               newAnno
-//               |> fun t ->
-
-//               t |> setAnnotationState
-//               t |> setLocalStorageAnnotation "Annotations"
-//           else 
-//               ()
-//           Browser.Dom.window.getSelection().removeAllRanges()
-//       )
-
 module ContextMenu =
         
+    let addAnnotation (state, setState) =
+      let term = window.getSelection().ToString().Trim()
+      Browser.Dom.console.log("addAnnotation", term)
+      if term.Length <> 0 then 
+          let newAnno = ({Key = term }::state) //errors are defined in Builder.fs
+          newAnno
+          |> setState
+      else 
+          ()
+
     [<ReactComponent>]
-    let private Menu (mousex: int, mousey: int) (resetter: unit-> unit) =
+    let private Menu (mousex: int, mousey: int) annotationState setAnnotationState (resetter: unit-> unit) =
         let ele = React.useInputRef()
         React.useLayoutEffectOnce(fun _ -> 
           if ele.current.IsSome then ele.current.Value.focus() )
@@ -72,18 +64,34 @@ module ContextMenu =
             prop.className "fixed z-20 block bg-slate-800 rounded shadow"
             prop.style [ style.top mousey; style.left mousex ]
             // prop.onClick ContextHelper.addAnnotation
-            prop.custom ("focused", "true")
             prop.tabIndex 0
             prop.onBlur (fun _ -> resetter() )
             prop.children [
               Html.ul [
+                Html.li [
+                  prop.className "py-1 px-2 hover:bg-slate-700 transition-colors"
+                  prop.children [
+                    Html.button [
+                      prop.text "Add Annotation"
+                      prop.onMouseDown (fun e -> 
+                        e.stopPropagation()
+                        e.preventDefault()
+                        Browser.Dom.console.log("Clicked Add Annotation")
+                        addAnnotation(annotationState, setAnnotationState)
+                        resetter() 
+                      )
+                    ]
+                  ]
+                ]
                 for i in 0 .. 5 do
                   Html.li [
                     prop.className "py-1 px-2 hover:bg-slate-700 transition-colors"
                     prop.children [
                       Html.button [
                         prop.text $"Option {i}"
-                        prop.onClick (fun _ -> 
+                        prop.onMouseDown (fun e -> 
+                          e.stopPropagation()
+                          e.preventDefault()
                           Browser.Dom.console.log("Clicked Option " + i.ToString())
                           resetter() 
                         )
@@ -99,29 +107,15 @@ module ContextMenu =
         location = (0,0)
     }
 
-    let ContextMenu (modalContext:DropdownModal) = 
+    let ContextMenu (modalContext:DropdownModal, annotationState, setAnnotationState: Annotation list -> unit) = 
         let resetter = fun () -> modalContext.setter initialModal //add actual function
         // let rmv = modalContext.setter initialModal 
-        Menu modalContext.modalState.location resetter
+        Menu modalContext.modalState.location annotationState setAnnotationState resetter
 
 type Builder =
+
     [<ReactComponent>]
-
     static member Main() =
-        let isLocalStorageClear (key:string) () =
-            match (Browser.WebStorage.localStorage.getItem key) with
-            | null -> true // Local storage is clear if the item doesn't exist
-            | _ -> false //if false then something exists and the else case gets started
-
-        let initialInteraction (id: string) =
-            if isLocalStorageClear id () = true then []
-            else Json.parseAs<Annotation list> (Browser.WebStorage.localStorage.getItem id)  
-
-        let (AnnotationState: Annotation list, setAnnotationState) = React.useState (initialInteraction "Annotations")
-
-        let setLocalStorageAnnotation (id: string)(nextAnnos: Annotation list) =
-            let JSONString = Json.stringify nextAnnos 
-            Browser.WebStorage.localStorage.setItem(id, JSONString)
 
         let modalContext = React.useContext (ModalContext.createModalContext)
 
@@ -159,9 +153,24 @@ type View =
             setter = setModal
             }
 
+        let isLocalStorageClear (key:string) () =
+            match (Browser.WebStorage.localStorage.getItem key) with
+            | null -> true // Local storage is clear if the item doesn't exist
+            | _ -> false //if false then something exists and the else case gets started
+
+        let initialInteraction (id: string) =
+            if isLocalStorageClear id () = true then []
+            else Json.parseAs<Annotation list> (Browser.WebStorage.localStorage.getItem id)  
+
+        let (AnnotationState: Annotation list, setAnnotationState) = React.useState (initialInteraction "Annotations")
+
+        let setLocalStorageAnnotation (id: string)(nextAnnos: Annotation list) =
+            let JSONString = Json.stringify nextAnnos 
+            Browser.WebStorage.localStorage.setItem(id, JSONString)
+
         let modalactivator = 
             match modalState.isActive with
-            |true -> ContextMenu.ContextMenu (myModalContext)
+            |true -> ContextMenu.ContextMenu (myModalContext, AnnotationState, setAnnotationState)
             |false -> Html.none
         
         let currentpage,setpage = React.useState(Types.Page.Builder) 
@@ -170,6 +179,12 @@ type View =
             Html.div [
                 prop.id "contentView"
                 prop.children [
+                    Html.div [
+                        for ele in AnnotationState do
+                            Html.div [
+                                prop.text ele.Key
+                            ]
+                    ]
                     match currentpage with
                     |Types.Page.Builder -> Builder.Main()
                     |Types.Page.Contact -> Html.div []
